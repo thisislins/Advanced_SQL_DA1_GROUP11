4d4ee886c665ec3cb6e21cf8494d220ecb09dec3
KariusHan4401 <lkhanh4401@gmail.com>
2021-10-24 16:15:39 +0700

    3c 3d

---
+++ Đồ án 1/DA_1_CSDLNC.sql
@@ -0,0 +1,12 @@
+﻿use QuanLyHoaDon
+go
+
+--Danh sách các sản phẩm có số lượng tồn < 100
+select sp.MASP, sp.TENSP, sp.SOLUONGTON
+from SANPHAM sp
+where sp.SOLUONGTON < 100
+
+--Danh sách các sản phẩm có giá trong một khoảng từ 30000 đến 40000
+select sp.MASP, sp.TENSP, sp.GIA
+from SANPHAM sp
+where sp.GIA > 30000 AND sp.GIA < 40000
