af0d8da6f72ce177c22babd129ae17ac2a66027b
KariusHan4401 <lkhanh4401@gmail.com>
2021-10-31 15:19:38 +0700

    Updated query c&d

--- Đồ án 1/DA_1_CSDLNC.sql
+++ Đồ án 1/DA_1_CSDLNC.sql
@@ -10,7 +10,39 @@ select sp.MASP, sp.TENSP, sp.SOLUONGTON
 from SANPHAM sp
 where sp.SOLUONGTON < 100
 
+select sp.MASP, sp.TENSP, sp.SOLUONGTON
+from SANPHAM sp
+where sp.SOLUONGTON not in (
+	select sp.SOLUONGTON
+	from SANPHAM sp
+	where sp.SOLUONGTON >= 100)
+
+select sp.MASP, sp.TENSP, sp.SOLUONGTON
+from SANPHAM sp
+where not exists (
+	select *
+	from SANPHAM sp2
+	where sp.MASP = sp2.MASP and sp.SOLUONGTON >= 100)
+
 --Danh sách các sản phẩm có giá trong một khoảng từ 30000 đến 40000
 select sp.MASP, sp.TENSP, sp.GIA
 from SANPHAM sp
-where sp.GIA > 30000 AND sp.GIA < 40000
+where sp.GIA >= 30000 AND sp.GIA <= 40000
+
+select sp.MASP, sp.TENSP, sp.GIA
+from SANPHAM sp
+where sp.GIA BETWEEN 30000 AND 40000
+
+select sp.MASP, sp.TENSP, sp.GIA
+from SANPHAM sp
+where sp.GIA not in (
+	select sp.GIA
+	from SANPHAM sp
+	where sp.GIA > 40000 OR sp.GIA < 30000)
+
+select sp.MASP, sp.TENSP, sp.GIA
+from SANPHAM sp
+where not exists (
+	select *
+	from SANPHAM sp2
+	where sp.MASP = sp2.MASP and (sp.GIA > 40000 OR sp.GIA < 30000))
