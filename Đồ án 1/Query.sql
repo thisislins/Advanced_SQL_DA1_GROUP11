effe7447e02bece8de856d0f242e2c2262cebfce
thisislins <lephuonglinh169@gmail.com>
2021-10-31 15:46:10 +0700

    Shortcut trigger

---
+++ Đồ án 1/Query.sql
@@ -0,0 +1,57 @@
+﻿USE QuanLyHoaDon
+GO
+
+--3a. Các hóa đơn lập trong năm 2020
+SELECT * 
+FROM HOADON
+WHERE year(NGAYLAP) = 2020
+GO
+
+-- 3b. Danh sách các khách hàng ở TP.HCM
+SELECT *
+FROM KHACHHANG
+WHERE TPHO LIKE N'TP. Hồ Chí Minh'
+GO
+
+--3c. Danh sách các sản phẩm có giá trong một khoảng từ 30000 đến 40000
+select sp.MASP, sp.TENSP, sp.GIA
+from SANPHAM sp
+where sp.GIA > 30000 AND sp.GIA < 40000
+GO
+
+--3d. Danh sách các sản phẩm có số lượng tồn < 100
+select sp.MASP, sp.TENSP, sp.SOLUONGTON
+from SANPHAM sp
+where sp.SOLUONGTON < 100
+GO
+
+
+-- 3e. Danh sách sản phẩm bán chạy nhất.
+SELECT * 
+FROM SANPHAM AS SP
+WHERE SP.MASP IN (SELECT MASP
+				FROM CT_HOADON
+				GROUP BY MASP
+				HAVING SUM(SOLUONG) >= ALL(
+							SELECT SUM(CT.SOLUONG)
+							FROM CT_HOADON AS CT
+							GROUP BY MASP
+							) 
+				)
+
+
+GO
+
+-- 3f. Sản phẩm có doanh thu cao nhất.
+SELECT * 
+FROM SANPHAM AS SP
+WHERE SP.MASP IN ( SELECT MASP
+				FROM CT_HOADON
+				GROUP BY MASP
+				HAVING SUM(THANHTIEN) >= ALL(
+							SELECT SUM(CT.THANHTIEN)
+							FROM CT_HOADON AS CT
+							GROUP BY MASP
+							) 
+				)
+
--- Đồ án 1/SourceCode/QuanLyHoaDon/QuanLyHoaDon/ThongKeHDControl.Designer.cs
+++ Đồ án 1/SourceCode/QuanLyHoaDon/QuanLyHoaDon/ThongKeHDControl.Designer.cs
@@ -44,28 +44,28 @@ namespace QuanLyHoaDon
             // 
             this.labelTitle.AutoSize = true;
             this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelTitle.Location = new System.Drawing.Point(256, 14);
+            this.labelTitle.Location = new System.Drawing.Point(293, 19);
             this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new System.Drawing.Size(215, 30);
+            this.labelTitle.Size = new System.Drawing.Size(274, 37);
             this.labelTitle.TabIndex = 0;
             this.labelTitle.Text = "Thống kê doanh thu";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(113, 169);
+            this.dataGridView1.Location = new System.Drawing.Point(129, 225);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(486, 241);
+            this.dataGridView1.Size = new System.Drawing.Size(555, 321);
             this.dataGridView1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(527, 89);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(602, 119);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(72, 22);
+            this.button1.Size = new System.Drawing.Size(82, 29);
             this.button1.TabIndex = 3;
             this.button1.Text = "Tìm";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,26 +74,23 @@ namespace QuanLyHoaDon
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(270, 89);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker1.Location = new System.Drawing.Point(309, 119);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(234, 23);
+            this.dateTimePicker1.Size = new System.Drawing.Size(267, 27);
             this.dateTimePicker1.TabIndex = 4;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(270, 130);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(309, 173);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 23);
+            this.textBox1.Size = new System.Drawing.Size(267, 27);
             this.textBox1.TabIndex = 5;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(527, 130);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(602, 173);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(72, 22);
+            this.button2.Size = new System.Drawing.Size(82, 29);
             this.button2.TabIndex = 6;
             this.button2.Text = "Tìm";
             this.button2.UseVisualStyleBackColor = true;
@@ -103,9 +100,9 @@ namespace QuanLyHoaDon
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(113, 132);
+            this.label1.Location = new System.Drawing.Point(129, 176);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 19);
+            this.label1.Size = new System.Drawing.Size(167, 23);
             this.label1.TabIndex = 7;
             this.label1.Text = "Các tháng theo năm";
             // 
@@ -113,15 +110,15 @@ namespace QuanLyHoaDon
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(113, 91);
+            this.label2.Location = new System.Drawing.Point(129, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 19);
+            this.label2.Size = new System.Drawing.Size(180, 23);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Trong tháng từ ngày";
+            this.label2.Text = "Trong tháng đến ngày";
             // 
             // ThongKeHDControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -131,8 +128,9 @@ namespace QuanLyHoaDon
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.labelTitle);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ThongKeHDControl";
-            this.Size = new System.Drawing.Size(697, 489);
+            this.Size = new System.Drawing.Size(797, 652);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
